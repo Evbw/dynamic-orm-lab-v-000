0b39d4af1120a88fff4759921e6962d3ed7c99f1
Everett <everett.bowline@gmail.com>
2018-06-05 01:32:42 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,8 +55,7 @@ class InteractiveRecord
   
   def self.find_by(hash)
   value = hash.values.first
-    
-    sql = "SELECT * FROM #{self.table_name} WHERE #{hash.keys.first} = '#{name}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{hash.keys.first} = '#{value}'"
     DB[:conn].execute(sql)
   end
 
